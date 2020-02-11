7d9eb475bd892a465db632de489538a541ab5557
89605502155 <47472417+89605502155@users.noreply.github.com>
2020-02-12 01:05:38 +0300

    ttfg

--- ii.py
+++ ii.py
@@ -2,8 +2,11 @@ import numpy as np
 import pandas as pd
 import tensorly as tl
 import os
-dataset =pd.read_csv('C:/Users/admin/Desktop/курсовая 2.0/X — копия/01.txt', sep= ' \t')
-dataset =  dataset.fillna( 0)
+y =pd.read_csv('C:/Users/admin/Desktop/курсовая 2.0/y.txt', sep= '\t')
+y =  y.fillna(0)
+y = y.replace(',','.', regex=True)
+y['Gominy'] = y.loc[:, 'Pahokee peat':'Disser'].sum(axis=1)
+print(y)
 fds = sorted(os.listdir('C:/Users/admin/Desktop/курсовая 2.0/X — копия/')) #формируем список файлов для считывания в цикле
 print(fds)
 k = 0
@@ -11,11 +14,8 @@ k = 0
 while k < 35:
     dataset = pd.read_csv('C:/Users/admin/Desktop/курсовая 2.0/X — копия/'+fds[k],sep='\t')
     dataset = dataset.fillna(0)
-    da=dataset.sum(axis = 0, skipna = True)
-    da=dataset.agg(['sum','mean'])
-    da=dataset.describe()
     k = k + 1
-    print(da)
+    print()
     print()
     print(k)
     print()
